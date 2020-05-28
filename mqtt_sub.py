a67bccd17f2bc22d28738ad8123880120a376a9c
boy07132004 <boy07132004@gmail.com>
2020-05-28 14:50:08 +0800

    Add sub

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -1,3 +1,9 @@
+from configparser import ConfigParser
+cfg = ConfigParser()
+cfg.read('config.ini')
+
+##############################################
+
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata,flags, rc):
@@ -6,12 +12,11 @@ def on_connect(client, userdata,flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic," ",msg.payload.decode('utf-8'))
 
-
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.username_pw_set('raspi3b',password='ml6a01')
-client.connect("192.168.0.111",1883,60)
+client.username_pw_set(cfg['Account']['USER'],password=cfg['Account']['PASSWORD'])
+client.connect(cfg['Others']['IP'],1883,60)
 
 client.loop_forever()
