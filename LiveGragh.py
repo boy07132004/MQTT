f0f3c9942ccfe01181834a7e3ddbe54b8ff33818
boy07132004 <boy07132004@gmail.com>
2020-07-30 08:59:36 +0800

    LiveGraph with MQTT

--- LiveGragh.py
+++ LiveGragh.py
@@ -1,6 +1,26 @@
+from collections import deque
+import paho.mqtt.client as mqtt
 import matplotlib.pyplot as plt
 import matplotlib.animation as ani
-from collections import deque
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe('xyz')
+    
+def on_message(client, userdata, msg):
+    _x,_y,_z = msg.payload.decode('utf-8').split(',')
+    global x
+    global y
+    global z
+    for i in ('x','y','z'):
+        eval(i).popleft()
+        eval(i).append(float(eval('_'+i)))
+        
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set('raspi3b', 'ml6a01')
+client.connect("192.168.0.161", 1883, 60)
 
 x = deque([0]*200)
 y = deque([0]*200)
@@ -9,30 +29,21 @@ z = deque([0]*200)
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 xs  = list(range(200)) 
-ax1.set_ylim([-4,4])
-line_x, = ax1.plot(xs,x)
-line_y, = ax1.plot(xs,y)
-line_z, = ax1.plot(xs,z)
-import random 
-def rand():
-    global x
-    global y
-    global z
-    while 1:
-        for i,j in ((x,-2),(y,0),(z,2)):
-            i.popleft()
-            i.append(j+random.random())
-        time.sleep(0.01)
-            
+ax1.set_ylim([-2.5,2.5])
+line_x, = ax1.plot(xs,x,'r')
+line_y, = ax1.plot(xs,y,'g')
+line_z, = ax1.plot(xs,z,'b')
+ax1.legend([line_x,line_y,line_z],['X','Y','Z'])
+
 
 def animate(i):
     for line,ls in ((line_x,x),(line_y,y),(line_z,z)):
         line.set_ydata(ls)
-    return 0#line_x,line_y,line_z
+    return 0
 
-import threading
-import time
 if __name__ == "__main__":
-    threading.Thread(target=rand).start()
-    ani = ani.FuncAnimation(fig,animate,interval=10)
+    ani = ani.FuncAnimation(fig,animate,interval=50)
+    client.loop_start()
     plt.show()
+    client.loop_stop()
+    client.disconnect()
